c67a403e45bbf2f56df1f83cbbb77ec732a2d01a
heytehm <44373701+heytehm@users.noreply.github.com>
2018-11-12 22:34:38 +0100

    Add files via upload

---
+++ PHP-COOKIE/scriptPhp.php
@@ -0,0 +1,33 @@
+
+
+<?php
+$temps = 3;
+$cookie_name = "choixcss";
+
+setcookie ($cookie_name, time() + $temps);
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+<?php
+if(isset($_COOKIE[$cookie_name])) {
+    echo  "<link rel=\"stylesheet\" href=\"defaut.css\">";
+} else {
+    echo  " <link rel=\"stylesheet\" href=\"special.css\">";
+}
+?>
+
+
+
+
+</head>
+<body>
+
+<h1 class="hdefault hspecial">Lorem.</h1>
+<p class="pdefault pspecial">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsam ab inventore vitae alias laboriosam exercitationem
+    provident nemo quis blanditiis repellat vero porro earum at, quod adipisci accusantium obcaecati aperiam
+    temporibus.</p>
+
+
+</body>
+</html>
